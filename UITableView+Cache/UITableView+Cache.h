a3a8061fd559f4dc653cbf5bc7e0f65f077a19e8
Maxim Gubin <maxim@kilograpp.com>
2016-06-19 13:21:29 +0300

    Project for Travis CI added

---
+++ UITableView+Cache/UITableView+Cache.h
@@ -0,0 +1,19 @@
+//
+//  UITableView+Cache.h
+//  UITableView+Cache
+//
+//  Created by Maxim Gubin on 19/06/16.
+//  Copyright © 2016 Kilograpp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UITableView+Cache.
+FOUNDATION_EXPORT double UITableView_CacheVersionNumber;
+
+//! Project version string for UITableView+Cache.
+FOUNDATION_EXPORT const unsigned char UITableView_CacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UITableView_Cache/PublicHeader.h>
+
+#import <UITableView_Cache/UITableView+Cache.h>
